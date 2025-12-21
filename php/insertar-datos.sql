409c909ad561ece241a699ea3edda785f4398d3c
UO265217 <uo265217@uniovi.es>
2025-12-21 13:19:01 +0100

    Cambios de preguntas

--- php/insertar-datos.sql
+++ php/insertar-datos.sql
@@ -8,10 +8,10 @@ INSERT INTO dispositivos (nombre_dispositivo) VALUES
 -- 2. Insertar las 10 preguntas del test sobre MotoGP
 INSERT INTO preguntas (enunciado, respuesta_a, respuesta_b, respuesta_c, respuesta_d, correcta) VALUES
 ('¿Cuál es el dorsal con el que corre Somkiat Chantra en MotoGP 2025?', '93', '35', '46', '12', 'b'),
-('¿Cuál es el gentilico días dura el calentamiento?', '1', '2', '3', '4', 'c'),
+('¿Cuántos días dura el calentamiento?', '1', '2', '3', '4', 'c'),
 ('¿Qué archivo específico debes cargar en la sección "Circuito" para ver la altimetría?', 'mapa.svg', 'altimetria.xml', 'altimetria.svg', 'circuito.png', 'c'),
 ('¿Qué marca de motos NO aparece en las cartas del Juego de Memoria?', 'Kawasaki', 'Ducati', 'KTM', 'Aprilia', 'a'),
-('¿Qué % de humedad había el día de la carrera a las 15 de la tarde?', '33%', '39%', '58.46 %', '48%', 'a'),
+('¿Qué % de humedad había el día de la carrera a las 15 de la tarde?', '33%', '39%', '58.46%', '48%', 'a'),
 ('¿Cuál es la altura del piloto Somkiat Chantra según su ficha técnica?', '1,68m', '1,75m', '1,71m', '1,80m', 'c'),
 ('¿Cuántas noticias aparecen en la pantalla de inicio?', '1', '2', '3', '4', 'c'),
 ('¿Qué archivo KML se solicita cargar para visualizar el mapa del circuito?', 'mapa.kml', 'track.kml', 'circuito.kml', 'motogp.kml', 'c'),
